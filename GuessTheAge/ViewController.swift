95a697a6b12e2c77032f433529a662a2d44e0328
Yury Lebedev <quasaryy@gmail.comu>
2023-08-10 21:01:26 +0400

    Initial Commit

---
+++ GuessTheAge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessTheAge
+//
+//  Created by Yury on 10/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
